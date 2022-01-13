9b751c38afade667e5eceafcf1c867ccf676e18b
Henry Solberg <milkshakeiii@gmail.com>
2022-01-12 22:14:55 -0800

    squares! mouse detection

--- game.py
+++ game.py
@@ -26,10 +26,60 @@ class Display:
         self.play_screen = pygame.Surface(GAME_SHAPE)
         
     def draw(self):
-        x = (self.screen.get_width() - self.play_screen.get_width()) // 2
-        y = (self.screen.get_height() - self.play_screen.get_height()) // 2
-        self.screen.blit(self.play_screen, (x, y))
+        self.playtop_x = (self.screen.get_width() - self.play_screen.get_width()) // 2
+        self.playtop_y = (self.screen.get_height() - self.play_screen.get_height()) // 2
+        self.screen.blit(self.play_screen, (self.playtop_x, self.playtop_y))
         pygame.display.flip()
+
+    def playzone_mouse(self, screenzone_mouse):
+        return (screenzone_mouse[0] - self.playtop_x,
+                screenzone_mouse[1] - self.playtop_y)
+
+
+class DisplayBoard:
+    def __init__(self):
+        self.surface = pygame.Surface(RIGHT_WINDOW_SHAPE)
+        self.squares = dict()
+        self.light_square = load_image("light_square")
+        self.dark_square = load_image("dark_square")
+        self.highlight_square = load_image("highlight_square")
+        self.highlighted_square = None
+
+    def surface_for_square(self, x, y, highlighted=False):
+        surface = pygame.Surface((GRID_WIDTH, GRID_HEIGHT))
+        background_square = self.light_square if (x+y)%2==0 else self.dark_square
+        if (highlighted):
+            background_square = self.highlight_square
+        surface.blit(background_square, (0, 0))
+        if (x, y) in self.squares:
+            surface.blit(self.squares[(x, y)])
+        return surface
+
+    def resurface(self):
+        for x in range(43):
+            for y in range(30):
+                self.paint_square(x, y)
+        return self.surface
+
+    def paint_square(self, x, y, highlighted=False):
+        self.surface.blit(self.surface_for_square(x, y, highlighted=highlighted),
+                          (x*GRID_WIDTH, y*GRID_HEIGHT))
+
+    def highlight(self, mouse_position):
+        mouse_board_position = (mouse_position[0] - TOP_LEFT_WINDOW_SHAPE[0],
+                                mouse_position[1])
+        if self.highlighted_square != None:
+            self.paint_square(self.highlighted_square[0],
+                              self.highlighted_square[1])
+        if self.surface.get_rect().collidepoint(mouse_board_position):
+            self.highlighted_square = (mouse_board_position[0]//GRID_WIDTH,
+                                       mouse_board_position[1]//GRID_HEIGHT)
+            self.paint_square(self.highlighted_square[0],
+                              self.highlighted_square[1],
+                              highlighted=True)
+        else:
+            self.highlighted_square = None
+            
 
 
 
@@ -43,23 +93,24 @@ if __name__=='__main__':
     bottom_left_window = pygame.Surface(BOTTOM_LEFT_WINDOW_SHAPE)
     bottom_left_window.fill((30, 60, 30))
 
-    right_window = pygame.Surface(RIGHT_WINDOW_SHAPE)
-    right_window.fill((30, 30, 60))
-    light_square = load_image("light_square")
-    dark_square = load_image("dark_square")
-    for x in range(43):
-        for y in range(30):
-            square = light_square if (x+y)%2==0 else dark_square
-            right_window.blit(square, (x*GRID_WIDTH, y*GRID_HEIGHT))
-
-    display.play_screen.blit(top_left_window, (0, 0))
-    display.play_screen.blit(bottom_left_window, (0, TOP_LEFT_WINDOW_SHAPE[1]))
-    display.play_screen.blit(right_window, (TOP_LEFT_WINDOW_SHAPE[0], 0))
+    display_board = DisplayBoard()
+    display_board.resurface()
 
     display.draw()
 
     
     while True:
+
+        playzone_mouse = display.playzone_mouse(pygame.mouse.get_pos())
+        display_board.highlight(playzone_mouse)
+
+        display.play_screen.blit(top_left_window, (0, 0))
+        display.play_screen.blit(bottom_left_window,
+                                 (0, TOP_LEFT_WINDOW_SHAPE[1]))
+        display.play_screen.blit(display_board.surface,
+                                 (TOP_LEFT_WINDOW_SHAPE[0], 0))
+        display.draw()
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
